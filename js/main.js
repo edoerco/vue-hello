0879c2604021380f675be6520e9678ab4656c0d5
Edoardo <ercolino.edoardo@gmail.com>
2021-09-06 15:10:05 +0200

    Bonus

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ Vue.config.devtools = true;
 let root = new Vue ({
     el: '#root',
     data: {
-        message: 'Ciao, mi chiamo Edoardo'
+        message: 'Ciao, mi chiamo',
+        nome: 'Edoardo',
+        pic: 'https://images.corsidia.com/schools/logos/000/000/263/original/boolean-logo.png?1615392366'
     }
 })
